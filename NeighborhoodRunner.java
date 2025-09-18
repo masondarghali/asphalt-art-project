d1edc39f96bcb34047f1e60f2d716deaea36b148
masondarghali <mdarghali2009@gmail.com>
2025-09-18 13:50:07 -0700

    finished project

---
+++ NeighborhoodRunner.java
@@ -0,0 +1,287 @@
+import org.code.neighborhood.*;
+
+public class NeighborhoodRunner {
+  public static void main(String[] args) {
+
+PainterPlus Mason = new PainterPlus();
+// The line of code above me summons a new painter named Mason. Mason's job is to paint the background.
+
+  while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+//These 4 lines of code above will make sure Mason fully paints the back round. I use a while loop so I can save lines and use less lines of code.
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+
+  while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+    while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnLeft();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnLeft();
+ while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+  Mason.turnRight();
+  Mason.paint("cyan");
+  Mason.move();
+  Mason.turnRight();
+
+     while(Mason.canMove()){
+    Mason.paint("cyan");
+    Mason.move();
+  }
+ 
+// This spawns my second painter that will paint the apple.
+ApplePainter Jason = new ApplePainter();
+
+Jason.paintApple();
+
+    
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# asphalt-art-project
+# Unit 1 - Asphalt Art
+
+## Introduction
+
+Cities use asphalt art to improve public safety, inspire their residents and visitors, and brighten communities. Your goal is to create asphalt art to revitalize The Neighborhood and bring the community together with the help of the Painter.
+
+## Requirements
+
+Use your knowledge of object-oriented programming, algorithms, the problem solving process, and decomposition strategies to create asphalt art:
+- **Create a new subclass** – Create at least one new subclass of the PainterPlus class that is used for a component of the asphalt art design.
+- **Plan an algorithm** – Use the problem solving process and decomposition strategies to plan an algorithm that incorporates a combination of sequencing, selection, and/or iteration.
+- **Write a method** – Write at least one method in a PainterPlus subclass that contributes to a component of the asphalt art design.
+- **Document your code** – Use comments to explain the purpose of the methods and code segments.
+
+## Notes: Neighborhood & Painter Class
+
+This project was created on Code.org's JavaLab platform using the built-in Neighborhood GUI output. To test and edit this project you must build in Code.org's JavaLab with the Neighborhood GUI enabled. For reference to the Painter class documentation, [you can read more here.](https://studio.code.org/docs/ide/javalab/classes/Painter)
+
+## Output:
+
+![The output of my program](mural.png)
+
+## Reflection
+
+1. Describe your project.
+
+My project has 2 painters. There names are Mason and Jason. Mason is in charge of painting the cyan background. Jason is incharge of painging the red apple. Mason paints first, and once the background is finished, Jason starts working on the apple.
+2. What are two things about your project that you are proud of?
+
+I am proud of how I was able to make the code work. I am very proud of myself. I am also proud of the apple because it promotes healthy eating.
+3. Describe something you would improve or do differently if you had an opportunity to change something about your project.
+
+I can maybe make the apple mroe detailed. I can also do another fruit or item to show to more people.
